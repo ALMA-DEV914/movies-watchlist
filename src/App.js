c9066f87ac0ff43c232dc3f6032b47bf46267cbd
ALMA_DEV914 <aungonalna58@gamil.com>
2022-02-15 13:03:58 -0800

    add header logo and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
+import 'swiper/swiper.min.css';
+import'./assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Routes from './config/Routes';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-  App
-    </div>
+    <BrowserRouter>
+    <Route render={props => (
+      <>
+      <Header {...props}/>
+      <Routes/>
+      <Footer/>
+      </>
+    )}/>
+
+
+    </BrowserRouter>
   );
 }
 
